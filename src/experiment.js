37eb60cc2d6e53dc5fd8d5ab26d048e1771cd8d0
Dora Pruteanu <dorapruteanu@MacBook-Pro-von-Dora.local>
2024-12-21 13:33:50 +0100

    missing data for csv

--- src/experiment.js
+++ src/experiment.js
@@ -394,6 +394,7 @@ export async function run({ assetPaths, input = {}, environment, title, version,
               .then(() => {
                 console.log("File was successfully uploaded");
                 data.response = filename_for_upload;
+                data.fileName = filename_for_upload;
                 data.type="mic_input"; // Remove response data from RAM, we already saved it to the server.
               })
               .catch(() => console.log("File upload failed")); // Cancel experiment? Try Again?
